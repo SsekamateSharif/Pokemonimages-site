15fce9fd843b768cb36fdad1d4c06b1a99ac18f6
SsekamateSharif <ssekamatesharif66@gmail.com>
2026-09-17 15:45:45 +0300

    initial commit

--- index.js
+++ index.js
@@ -4,12 +4,18 @@
 let InputEl = document.getElementById("input-el");
 let ImgEL = document.querySelector("#Pokemonimg");
 async function fetchData(){ // the async keyword makes this function return a promise
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${InputEl.value}`); // we use the name that we typed in the textfield as the name for our pokemon
-    // we use await since this returns a promise 
-    const data = await response.json(); // converting the data retrieved into a json file. also returns a promise 
-    // getting the image source from the data json file retrieved 
-    let Imgsrc = data.sprites.front_default;
-    ImgEL.src = Imgsrc;
+    try{
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${InputEl.value}`); // we use the name that we typed in the textfield as the name for our pokemon
+        // we use await since this returns a promise 
+        const data = await response.json(); // converting the data retrieved into a json file. also returns a promise 
+        // getting the image source from the data json file retrieved 
+        let Imgsrc = data.sprites.front_default;
+        ImgEL.src = Imgsrc;
+    }
+    catch(error){
+        let errorEl = document.getElementById("error");
+        errorEl.textContent = "An Error occured: ", error;
+    }
 }
 
 // adding an event listener to the fetch data button 
